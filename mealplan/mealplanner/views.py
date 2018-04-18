4fffdc640234b875b4f4d4dc38d8f0385f3ba682
Petr Manas <peter@petermanas.com>
2018-04-18 19:45:23 +0100

    Basic view handlers created

--- mealplan/mealplanner/views.py
+++ mealplan/mealplanner/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Eventually, I would like to convert some of these either to Django premade
+# views or something handy of my own creation.
+# For now, let's stick with the basics.
+
+def index(request):
+    return HttpResponse("This is the index page.")
+
+def ingredients(request):
+    return HttpResponse("This page will contain the user's ingredients.")
+
+def recipes(request):
+    return HttpResponse("This page will contain the user's recipes.")
+
+def inventory(request):
+    return HttpResponse("This page will contain the user's inventory.")
+
+def planner(request):
+    return HttpResponse("This will serve the main purpose: planning your meals.")
