b22bb584e04a0c9d4f1d5a26135d04cf9a347be5
Petr Manas <peter@petermanas.com>
2018-04-24 11:26:52 +0100

    model: recipes, ingredients in recipes, and inventory

--- mealplan/mealplanner/models.py
+++ mealplan/mealplanner/models.py
@@ -31,7 +31,7 @@ class Unit(models.Model):
     # Shorthand name, e.g. kg, rashers
     shorthand = models.CharField(max_length=20)
     # Ingredient the unit belongs to. If null, it can be used for any ingredient (e.g. kg, oz)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, null=True)
+    ingredient = models.ForeignKey('Ingredient', on_delete=models.CASCADE, null=True)
 
 class Ingredient(models.Model):
     # Name of the ingredient, can be rather long if needed
@@ -57,3 +57,38 @@ class Ingredient(models.Model):
     price = models.FloatField(blank=True, default=0.0)
     # Description: either text description or a generated recipe (feature idea)
     description = models.TextField(blank=True, default="")
+
+class Inventory(models.Model):
+    # Ingredient which is in your inventory
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    # Quantity of that ingredient that you have (in unit_type)
+    quantity = models.FloatField()
+
+class Recipe(models.Model):
+    # Name of the recipe
+    name = models.CharField(max_length=100)
+    # Number of servings the recipe yields
+    servings = models.PositiveSmallIntegerField()
+    # Preparation time (in minutes)
+    prep_time = models.DurationField()
+    # Cooking time (in minutes)
+    cook_time = models.DurationField()
+    # Tags: e.g. mexican, spicy
+    tags = models.ManyToManyField(Tag)
+    # Ingredients this recipe composes of
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        through_fields=('recipe', 'ingredient'),
+    )
+    # Directions: can be text for now, maybe change to JSON later
+    directions = models.TextField()
+
+class RecipeIngredient(models.Model):
+    # The two foreign keys this is related to
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    # Unit used for display. If null, use base unit_type instead
+    unit = models.ForeignKey(Unit, on_delete=models.SET_NULL, null=True)
+    # Quantity of the ingredient in terms of its base unit_type
+    quantity = models.FloatField()
