ca17b1068786559fcb17196d71e289d6e761ba3f
Petr Manas <peter@petermanas.com>
2018-04-24 11:38:24 +0100

    registered models in admin (no fancy names)

--- mealplan/mealplanner/admin.py
+++ mealplan/mealplanner/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Tag, Nutrition, Unit, Ingredient, Inventory, Recipe
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Nutrition)
+admin.site.register(Unit)
+admin.site.register(Ingredient)
+admin.site.register(Inventory)
+admin.site.register(Recipe)
