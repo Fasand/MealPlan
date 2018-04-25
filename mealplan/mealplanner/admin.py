565d3ec620d8b54bd15be9984f9db6313f75bd64
Petr Manas <peter@petermanas.com>
2018-04-25 15:40:48 +0100

    model: added a few things to admin (recipe ingredients, ...)

--- mealplan/mealplanner/admin.py
+++ mealplan/mealplanner/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Category, Tag, Nutrition, Unit, Ingredient, Inventory, Recipe
+from .models import Category, Tag, Nutrition, Unit, Ingredient, Inventory, Recipe, RecipeIngredient
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 5
+
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Information", {"fields": ["name", "servings", "prep_time", "cook_time", "tags"]}),
+        ("Directions",          {"fields": ["directions"]}),
+    ]
+    inlines = [RecipeIngredientInline]
 
 admin.site.register(Category)
 admin.site.register(Tag)
@@ -8,4 +19,4 @@ admin.site.register(Nutrition)
 admin.site.register(Unit)
 admin.site.register(Ingredient)
 admin.site.register(Inventory)
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeAdmin)
