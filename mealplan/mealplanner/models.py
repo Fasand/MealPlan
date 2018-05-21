48fc0571d93154ea0a80548a73673a1e67a60b7f
Petr Manas <peter@petermanas.com>
2018-05-21 08:38:52 +0100

    flake8: fixed stylistic mistakes, looks beautiful now...

--- mealplan/mealplanner/models.py
+++ mealplan/mealplanner/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
 
 class Unit(models.Model):
     # Unit type: weight-based or volume-based (G or ML)
@@ -28,7 +31,8 @@ class Unit(models.Model):
     name = models.CharField(max_length=50)
     # Shorthand name, e.g. kg, rashers
     shorthand = models.CharField(max_length=20)
-    # Ingredient the unit belongs to. If null, it can be used for any ingredient (e.g. kg, oz)
+    # Ingredient the unit belongs to. If null, it can be used
+    # for any ingredient (e.g. kg, oz)
     belongs_to_ingredient = models.ForeignKey(
         'Ingredient',
         on_delete=models.CASCADE,
@@ -44,11 +48,14 @@ class Unit(models.Model):
     def __str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
     # Name of the ingredient, can be rather long if needed
     name = models.CharField(max_length=100)
     # Category: e.g. vegetables, spices, etc.
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 null=True)
     # Tags: e.g. mexican, spicy
     tags = models.ManyToManyField(Tag, blank=True)
     # Unit type: weight-based or volume-based (G or ML)
@@ -75,6 +82,7 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
+
 class Nutrition(models.Model):
     # Nutrition per 100 g/ml
     # More fields can be added in the future,
@@ -90,7 +98,9 @@ class Nutrition(models.Model):
     # ...
 
     # Ingredient to which it belongs
-    ingredient = models.OneToOneField(Ingredient, on_delete=models.CASCADE, null=True)
+    ingredient = models.OneToOneField(Ingredient,
+                                      on_delete=models.CASCADE,
+                                      null=True)
 
     def __str__(self):
         return "{:.0f}kcal, {:.1f}F, {:.1f}C, {:.1f}P".format(
@@ -106,7 +116,8 @@ class Nutrition(models.Model):
             if type(other) is int or type(other) is float:
                 return self
             else:
-                raise TypeError("Nutrition can be added only with other Nutritions")
+                raise TypeError("Nutrition can be added "
+                                "only with other Nutritions")
 
         # Create a new value dict with added values
         # Don't add ingredient and id -> would throw an error
@@ -118,9 +129,10 @@ class Nutrition(models.Model):
                 # Fields can be None, pick whichever isn't None
                 if f.value_from_object(other) is None:
                     added = f.value_from_object(self)
-                else: added = f.value_from_object(other)
+                else:
+                    added = f.value_from_object(other)
             tot_vals[f.name] = added
-    
+
         # Make sure ID and Ingredient aren't added
         tot_vals["id"] = 0
         tot_vals["ingredient"] = None
@@ -128,7 +140,7 @@ class Nutrition(models.Model):
         # Create a new Nutrition from the added values
         tot = Nutrition(**tot_vals)
         return tot
-    
+
     def __radd__(self, other):
         return self.__add__(other)
 
@@ -137,7 +149,7 @@ class Nutrition(models.Model):
             other = float(other)
         except ValueError:
             raise TypeError("Nutrition can only be multiplied a number")
-        
+
         # Create a new value dict with added values
         # Don't multiply ingredient and id -> would throw an error
         tot_vals = {}
@@ -148,7 +160,7 @@ class Nutrition(models.Model):
                 # It's ok if it's None
                 multiplied = None
             tot_vals[f.name] = multiplied
-        
+
         # Make sure ID and Ingredient aren't multiplied
         tot_vals["id"] = 0
         tot_vals["ingredient"] = None
@@ -156,7 +168,7 @@ class Nutrition(models.Model):
         # Create a new Nutrition from the multiplied values
         tot = Nutrition(**tot_vals)
         return tot
-    
+
     def __rmul__(self, other):
         return self.__mul__(other)
 
@@ -168,6 +180,7 @@ class Nutrition(models.Model):
 
         return self.__mul__(1.0 / other)
 
+
 class Inventory(models.Model):
     # Ingredient which is in your inventory
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
@@ -175,7 +188,9 @@ class Inventory(models.Model):
     quantity = models.FloatField()
 
     def __str__(self):
-        return "{}: {}{}".format(self.ingredient, self.quantity, self.ingredient.unit_type)
+        return "{}: {}{}".format(self.ingredient, self.quantity,
+                                 self.ingredient.unit_type)
+
 
 class Recipe(models.Model):
     # Name of the recipe
@@ -203,13 +218,16 @@ class Recipe(models.Model):
     def get_recipe_nutrition(self):
         nutritions = []
         for ri in self.recipeingredient_set.all():
-            nutritions.append(ri.ingredient.nutrition * (ri.unit.value / 100.0) * ri.quantity)
+            nutritions.append(ri.ingredient.nutrition
+                              * (ri.unit.value / 100.0)
+                              * ri.quantity)
         return sum(nutritions)
 
     def get_serving_nutrition(self, servings=None):
         if servings is None:
             servings = float(self.servings)
         return self.get_recipe_nutrition() / servings
+
 
 class RecipeIngredient(models.Model):
     # The two foreign keys this is related to
@@ -221,5 +239,6 @@ class RecipeIngredient(models.Model):
     quantity = models.FloatField()
 
     def __str__(self):
-        return "{} - {}: {:.1f} {}".format(self.recipe.name, self.ingredient.name,
+        return "{} - {}: {:.1f} {}".format(
+            self.recipe.name, self.ingredient.name,
             self.quantity, self.unit.shorthand)
--- mealplan/mealplanner/tests.py
+++ mealplan/mealplanner/tests.py
@@ -4,37 +4,38 @@ from datetime import timedelta
 
 from .models import Nutrition, Ingredient, Recipe, Unit
 
+
 class NutritionModelTests(TestCase):
-    
+
     # Addition
 
     def test_add_two_ingredient_nutritions(self):
         i1 = Ingredient(name="Apple", unit_type="g")
         i2 = Ingredient(name="Orange", unit_type="g")
         n1 = Nutrition(calories=300, fat=21.2, carbs=7.3, protein=5.1,
-            ingredient=i1, id=0)
+                       ingredient=i1, id=0)
         n2 = Nutrition(calories=210, fat=11.2, carbs=8.3, protein=4.1,
-            ingredient=i2, id=1)
+                       ingredient=i2, id=1)
         add = n1 + n2
         radd = n2 + n1
         self.assertEquals(add, radd)
 
     def test_add_two_noningredient_nutritions(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         n2 = Nutrition(calories=300, fat=21.2, carbs=7.3, protein=5.1,
-            ingredient=None, id=1)
+                       ingredient=None, id=1)
         add = n1 + n2
         radd = n2 + n1
         self.assertEquals(add, radd)
 
     def test_add_three_nutritions(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         n2 = Nutrition(calories=300, fat=21.2, carbs=7.3, protein=5.1,
-            ingredient=None, id=1)
+                       ingredient=None, id=1)
         n3 = Nutrition(calories=200, fat=11.2, carbs=5.7, protein=2.1,
-            ingredient=None, id=2)
+                       ingredient=None, id=2)
         add = n1 + n2 + n3
         radd = n3 + n2 + n1
         another = n2 + n1 + n3
@@ -44,25 +45,25 @@ class NutritionModelTests(TestCase):
 
     def test_sum_nutritions(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         n2 = Nutrition(calories=300, fat=21.2, carbs=7.3, protein=5.1,
-            ingredient=None, id=1)
+                       ingredient=None, id=1)
         n3 = Nutrition(calories=200, fat=11.2, carbs=5.7, protein=2.1,
-            ingredient=None, id=2)
+                       ingredient=None, id=2)
         ns = [n1, n2, n3]
         s = sum(ns)
         self.assertEquals(s, n1 + n2 + n3)
 
     def test_add_nutrition_and_number(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         # n + Nutrition should return Nutrition
         self.assertEquals(n1, n1 + 1.5)
         self.assertEquals(n1, 1.5 + n1)
 
     def test_add_nutrition_and_nonnumber(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         with self.assertRaises(TypeError):
             n1 + []
         with self.assertRaises(TypeError):
@@ -70,19 +71,19 @@ class NutritionModelTests(TestCase):
 
     def test_add_nutritions_with_none(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=1, fat_saturated=2.5)
+                       ingredient=None, id=1, fat_saturated=2.5)
         n2 = Nutrition(calories=200, fat=11.2, carbs=5.7, protein=2.1,
-            ingredient=None, id=2, fiber=3.3)
+                       ingredient=None, id=2, fiber=3.3)
         n3 = Nutrition(calories=300, fat=21.2, carbs=11, protein=4.2,
-            ingredient=None, id=0, fat_saturated=2.5, fiber=3.3)
+                       ingredient=None, id=0, fat_saturated=2.5, fiber=3.3)
         self.assertEquals(n1 + n2, n3)
         self.assertEquals(n2 + n1, n3)
-        
+
     # Multiplication
 
     def test_multiply_nutrition_by_nonnumber(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         with self.assertRaises(TypeError):
             n1 * n1
         with self.assertRaises(TypeError):
@@ -90,19 +91,19 @@ class NutritionModelTests(TestCase):
 
     def test_multiply_nutrition_by_one(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         self.assertEquals(n1, n1 * 1)
         self.assertEquals(n1, 1 * n1)
 
     def test_multiply_nutrition_by_two(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         self.assertEquals(n1 * 2, n1 + n1)
         self.assertEquals(2 * n1, n1 + n1)
 
     def test_multiply_nutrition_by_number(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         for i in range(0, 100):
             x = i / 10.0
             multiplied = n1 * x
@@ -112,22 +113,22 @@ class NutritionModelTests(TestCase):
     def test_multiply_ingredient_nutrition(self):
         i1 = Ingredient(name="Orange", unit_type="g", id=1)
         n1 = Nutrition(calories=300, fat=21.2, carbs=7.3, protein=5.1,
-            ingredient=i1, id=0)
+                       ingredient=i1, id=0)
         self.assertEquals(n1 * 2.0, 2.0 * n1)
 
     # Division
 
     def test_divide_nutrition_by_number(self):
         n1 = Nutrition(calories=250, fat=10, carbs=5.5, protein=10.5,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         n2 = Nutrition(calories=100.0, fat=4.0, carbs=2.2, protein=4.2,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         self.assertEquals(n1 / 2.5, n2)
         self.assertEquals(n1 / 2.5, n1 * (1/2.5))
 
     def test_divide_nutrition_by_nonnumber(self):
         n1 = Nutrition(calories=100, fat=10, carbs=5.3, protein=2.1,
-            ingredient=None, id=0)
+                       ingredient=None, id=0)
         with self.assertRaises(TypeError):
             n1 / n1
         with self.assertRaises(TypeError):
@@ -139,32 +140,39 @@ class RecipeModelTests(TestCase):
     def test_get_recipe_nutrition(self):
         apple = Ingredient(name="Apple", unit_type="g")
         apple.save()
-        apple_nutrition = Nutrition(calories=99, fat=0, carbs=10.2, protein=0.2,
-            ingredient=Ingredient.objects.get(name__contains="Apple"))
+        apple_nutrition = Nutrition(
+            calories=99, fat=0, carbs=10.2, protein=0.2,
+            ingredient=Ingredient.objects.get(name__contains="Apple")
+        )
         apple_nutrition.save()
         orange = Ingredient(name="Orange", unit_type="g")
         orange.save()
-        orange_nutrition = Nutrition(calories=120, fat=0.1, carbs=14.2, protein=0.3,
-            ingredient=Ingredient.objects.get(name__contains="Orange"))
+        orange_nutrition = Nutrition(
+            calories=120, fat=0.1, carbs=14.2, protein=0.3,
+            ingredient=Ingredient.objects.get(name__contains="Orange")
+        )
         orange_nutrition.save()
 
         apples_and_oranges = Recipe(
-            name="Apples and Oranges", servings=1, prep_time = timedelta(minutes=2),
-            cook_time=timedelta(minutes=1), directions="do it")
+            name="Apples and Oranges", servings=1, directions="do it",
+            prep_time=timedelta(minutes=2), cook_time=timedelta(minutes=1)
+        )
         apples_and_oranges.save()
 
         grams = Unit(unit_type="g", value=1, name="grams", shorthand="g")
         grams.save()
 
         apples_and_oranges.recipeingredient_set.create(
-            ingredient=apple, quantity=80.0, unit=Unit.objects.get(name__contains="grams")
+            ingredient=apple, quantity=80.0,
+            unit=Unit.objects.get(name__contains="grams")
         )
         apples_and_oranges.recipeingredient_set.create(
-            ingredient=orange, quantity=160.0, unit=Unit.objects.get(name__contains="grams")
+            ingredient=orange, quantity=160.0,
+            unit=Unit.objects.get(name__contains="grams")
         )
 
         self.assertEquals(
             apples_and_oranges.get_recipe_nutrition(),
             Nutrition(calories=271.2, fat=0.16, carbs=30.88, protein=0.64,
-                ingredient=None, id=0)
+                      ingredient=None, id=0)
         )
