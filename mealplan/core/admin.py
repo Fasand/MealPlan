f626875e2d2a358c8bd969e0f1f35f56e76fc302
Fasand <peter@petermanas.com>
2020-04-20 12:28:39 +0200

    add admin for User

--- mealplan/core/admin.py
+++ mealplan/core/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 
 class BaseModelAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'created_by', 'modified', 'modified_by')
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
