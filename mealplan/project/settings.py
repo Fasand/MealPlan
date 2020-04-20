fadc3abdf8fa563fdc60291cf6f04ef8e5255cde
Fasand <peter@petermanas.com>
2020-04-20 11:14:05 +0200

    make local_settings the primary setting module

--- mealplan/project/settings.py
+++ mealplan/project/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 from django.utils.translation import ugettext_lazy as _
 
-# Keep local settings private
-# Includes: SECRET_KEY, TIME_ZONE
-# TODO: local settings are overriden by base settings !!!
-from .local_settings import *
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
